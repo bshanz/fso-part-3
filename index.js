876f3d3ffc10cd4a26001657339807503d34269c
“Brian <90475729+bshanz@users.noreply.github.com>
2023-07-15 12:54:10 -0400

    Exercise 3.11 done

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('dist'))
+
 console.log("Part 3.10 done")
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
