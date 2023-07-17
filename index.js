4e8eee5a81997db1c7ea3fe9beb604a61fd30594
“Brian <90475729+bshanz@users.noreply.github.com>
2023-07-17 19:13:52 -0400

    Updated

--- index.js
+++ index.js
@@ -9,29 +9,6 @@ app.use(cors())
 
 app.use(express.json())
 
-// const mongoose = require('mongoose')
-// //const password = 'TIU14dU1H5KIFqY2'
-const password = process.env.PASSWORD
-
-// const url =
-//   `mongodb+srv://brianshanley24:${password}@cluster0.um9s28l.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-// mongoose.set('strictQuery',false)
-// mongoose.connect(url)
-
-// const noteSchema = new mongoose.Schema({
-//   content: String,
-//   important: Boolean,
-// })
-
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
-// })
-
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 // Include the standard 'tiny' format information, plus the body
@@ -71,10 +48,6 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-// app.get('/api/notes', (request, response) => {
-//   response.json(notes)
-// })
-
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
@@ -82,16 +55,10 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-  
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
   })
+})
 
   app.delete('/api/notes/:id', (request, response) => {
     // Get the ID of the request
@@ -108,35 +75,22 @@ const generateId = () => {
     return randomNumber
 }
   
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-    console.log(body.content)
-    console.log(body.important)
-  
-    if (!body.content || !body.important) {
-      return response.status(400).json({ 
-        error: 'content or importance missing' 
-      })
-    }
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-    let exists = notes.some(note => note.content === body.content)
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-    if (exists){
-        return response.status(400).json({
-            error: "content must be unique"
-        })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
   })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
 
   const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
