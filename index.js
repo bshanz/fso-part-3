7ad826f8744d04ed468383fdb44455ea13085a47
“Brian <90475729+bshanz@users.noreply.github.com>
2023-07-19 14:43:27 -0400

    Updated exercises

--- index.js
+++ index.js
@@ -16,24 +16,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use(express.static('build'))
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -42,7 +24,7 @@ const requestLogger = (request, response, next) => {
     next()
   }
 
-  app.use(requestLogger)
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -54,26 +36,63 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    // Get the ID of the request
-    const id = Number(request.params.id)
-    // Filter the notes so that they no longer contain that ID
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 
 const generateId = () => {
     const randomNumber = Math.floor(Math.random() * 10001);
     return randomNumber
 }
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
   
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -92,12 +111,8 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
-
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
   const PORT = process.env.PORT
   app.listen(PORT, () => {
